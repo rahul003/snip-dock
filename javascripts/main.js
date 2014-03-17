317b4d37d8b71f4abac7b150ec3402a0ae20f96b
rahul003 <rahulhuilgol@gmail.com>
2014-03-17 23:40:19 +0530

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,6 @@
 console.log('This would be the main JS file.');
 
 $( document ).ready(function() {
-  $("#dynamic").append('<iframe src="http://ghbtns.com/github-btn.html?user=rahul003&repo=snip-dock&type=watch&count=true&size=large" allowtransparency="true" frameborder="0" scrolling="0" width="170" height="30"></iframe>');
+  ("#dynamic").append('<iframe src="http://ghbtns.com/github-btn.html?user=rahul003&repo=snip-dock&type=watch&count=true&size=large" allowtransparency="true" frameborder="0" scrolling="0" width="170" height="30"></iframe>');
 });  
 
