20e7922a2af4451ce80a43c46e35127df1ab2738
rahul003 <rahulhuilgol@gmail.com>
2014-03-17 23:18:51 +0530

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,4 @@
 console.log('This would be the main JS file.');
+
+  
+$("#dynamic").append('<iframe src="http://ghbtns.com/github-btn.html?user=rahul003&repo=snip-dock&type=watch&count=true&size=large" allowtransparency="true" frameborder="0" scrolling="0" width="170" height="30"></iframe>');
