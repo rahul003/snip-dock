b57af8f4c5634934cf484ac0d438313e4218d7e1
rahul003 <rahulhuilgol@gmail.com>
2014-03-18 18:21:27 +0530

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,5 @@
 jQuery(document).ready(function($) {
+  console.log("adding");
   $("#dynamic").append('<iframe src="http://ghbtns.com/github-btn.html?user=rahul003&repo=snip-dock&type=watch&count=true&size=large" allowtransparency="true" frameborder="0" scrolling="0" width="170" height="30"></iframe>');
 });  
 
