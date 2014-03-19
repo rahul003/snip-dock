7379215a8badffabfd5e03bee319646d1a6f5889
rahul003 <rahulhuilgol@gmail.com>
2014-03-19 18:53:49 +0530

    Update apis.js

--- javascripts/apis.js
+++ javascripts/apis.js
@@ -11,7 +11,7 @@ $.ajax({
         gitdl = results.data[0].assets[0].download_count;
         console.log(gitdl);
 
-        $("#downcount").append("<strong style='font-size:20px'> Has been downloaded " +gitdl+" times on Github </strong>");
+        $("#downcount").append("<strong style='font-size:20px'> Number of Downloads: " +gitdl+" on Github </strong>");
     	
     }
 });
